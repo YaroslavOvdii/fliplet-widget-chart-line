96474535447a52759f9858064a9917bc6e56c163
YaroslavOvdii <yaroslav.o@upplabs.com>
2019-12-24 10:24:32 +0200

    Changed to instance

--- js/build.js
+++ js/build.js
@@ -1,12 +1,11 @@
-(function(){
+(function () {
   window.ui = window.ui || {}
   ui.flipletCharts = ui.flipletCharts || {};
 
   function init() {
-    $('[data-chart-line-id]').each(function (i, el) {
-      var chartId = $(this).data('chart-line-id');
-      var data = Fliplet.Widget.getData(chartId);
-      var $container = $(el);
+    Fliplet.Widget.instance('chart-line', function (data) {
+      var chartId = data.id;
+      var $container = $(this);
       var refreshTimeout = 5000;
       var updateDateFormat = 'hh:mm:ss a';
 
@@ -17,13 +16,30 @@
 
       function refreshData() {
         if (typeof data.dataSourceQuery !== 'object') {
-          data.entries = [
-            {x: 1, y: 2},
-            {x: 2, y: 1.5},
-            {x: 3, y: 4},
-            {x: 4, y: 1},
-            {x: 5, y: 2},
-            {x: 6, y: 2.5}
+          data.entries = [{
+              x: 1,
+              y: 2
+            },
+            {
+              x: 2,
+              y: 1.5
+            },
+            {
+              x: 3,
+              y: 4
+            },
+            {
+              x: 4,
+              y: 1
+            },
+            {
+              x: 5,
+              y: 2
+            },
+            {
+              x: 6,
+              y: 2.5
+            }
           ];
           data.xAxisTitle = 'X-axis';
           data.yAxisTitle = 'Y-axis';
@@ -31,15 +47,15 @@
           return Promise.resolve()
         }
 
-        return Fliplet.Hooks.run('beforeQueryChart', data.dataSourceQuery).then(function() {
+        return Fliplet.Hooks.run('beforeQueryChart', data.dataSourceQuery).then(function () {
           return Fliplet.DataSources.fetchWithOptions(data.dataSourceQuery)
-        }).then(function(result){
-          return Fliplet.Hooks.run('afterQueryChart', result).then(function() {
+        }).then(function (result) {
+          return Fliplet.Hooks.run('afterQueryChart', result).then(function () {
             resetData();
             if (result.dataSource.columns.indexOf(data.dataSourceQuery.columns.xAxis) < 0 || result.dataSource.columns.indexOf(data.dataSourceQuery.columns.yAxis) < 0) {
               return Promise.resolve();
             }
-            result.dataSourceEntries.forEach(function(row) {
+            result.dataSourceEntries.forEach(function (row) {
               var x;
               if (data.dataFormat === 'timestamp') {
                 x = new Date(row[data.dataSourceQuery.columns.xAxis] || 0).getTime()
@@ -49,14 +65,14 @@
               var y = parseInt(row[data.dataSourceQuery.columns.yAxis], 10) || 0;
               data.entries.push([x, y]);
             });
-            data.entries = _.sortBy(data.entries, function(entry){
+            data.entries = _.sortBy(data.entries, function (entry) {
               return entry[0];
             });
             // SAVES THE TOTAL NUMBER OF ROW/ENTRIES
             data.totalEntries = data.entries.length;
 
             return Promise.resolve();
-          }).catch(function(error){
+          }).catch(function (error) {
             return Promise.reject(error);
           });
         })
@@ -78,8 +94,8 @@
       }
 
       function getLatestData() {
-        setTimeout(function(){
-          refreshData().then(function(){
+        setTimeout(function () {
+          refreshData().then(function () {
             refreshChart();
             if (data.autoRefresh) {
               getLatestData();
@@ -93,11 +109,11 @@
           '#00abd1', '#ed9119', '#7D4B79', '#F05865', '#36344C',
           '#474975', '#8D8EA6', '#FF5722', '#009688', '#E91E63'
         ];
-        colors.forEach(function eachColor (color, index) {
+        colors.forEach(function eachColor(color, index) {
           if (!Fliplet.Themes) {
             return;
           }
-          colors[index] = Fliplet.Themes.Current.get('chartColor'+(index+1)) || color;
+          colors[index] = Fliplet.Themes.Current.get('chartColor' + (index + 1)) || color;
         });
         var chartOpt = {
           chart: {
@@ -108,7 +124,7 @@
               fontFamily: (Fliplet.Themes && Fliplet.Themes.Current.get('bodyFontFamily')) || 'sans-serif'
             },
             events: {
-              load: function(){
+              load: function () {
                 refreshChartInfo();
                 if (data.autoRefresh) {
                   getLatestData();
@@ -129,7 +145,7 @@
               enabled: data.xAxisTitle !== ''
             },
             labels: {
-              formatter: function(){
+              formatter: function () {
                 if (data.dataFormat === 'timestamp') {
                   return moment(this.value).format('YYYY-MM-DD');
                 }
@@ -156,17 +172,17 @@
             headerFormat: '',
             pointFormat: [
               '<strong>',
-              (data.xAxisTitle !== ''
-                ? data.xAxisTitle
-                : data.dataSourceQuery.columns.xAxis),
+              (data.xAxisTitle !== '' ?
+                data.xAxisTitle :
+                data.dataSourceQuery.columns.xAxis),
               '</strong> ',
-              (data.dataFormat === 'timestamp'
-                ? '{point.x:%Y-%m-%d %H:%M:%S}'
-                : '{point.x}'),
+              (data.dataFormat === 'timestamp' ?
+                '{point.x:%Y-%m-%d %H:%M:%S}' :
+                '{point.x}'),
               '<br><strong>',
-              (data.yAxisTitle !== ''
-                ? data.yAxisTitle
-                : data.dataSourceQuery.columns.yAxis),
+              (data.yAxisTitle !== '' ?
+                data.yAxisTitle :
+                data.dataSourceQuery.columns.yAxis),
               '</strong>: {point.y}'
             ].join('')
           },
@@ -212,14 +228,16 @@
         ui.flipletCharts[chartId].reflow();
       });
 
-      refreshData().then(drawChart).catch(function(error){
+      refreshData().then(drawChart).catch(function (error) {
         console.error(error);
       });
     });
   }
 
-  Fliplet().then(function(){
-    var debounceLoad = _.debounce(init, 500, { leading: true });
+  Fliplet().then(function () {
+    var debounceLoad = _.debounce(init, 500, {
+      leading: true
+    });
     Fliplet.Studio.onEvent(function (event) {
       if (event.detail.event === 'reload-widget-instance') {
         debounceLoad();
@@ -228,4 +246,4 @@
 
     init();
   });
-})();
+})()
